fdba869d7024a03f0052edba1b1c5416ded879b5
Omar M Juarez <omarmju906p@hotmail.com>
2022-09-16 22:36:51 -0500

    Stream to Collect.

--- modules/src/main/java/com/omju/excercises/streams/StreamPractice.java
+++ modules/src/main/java/com/omju/excercises/streams/StreamPractice.java
@@ -1,14 +1,19 @@
 package com.omju.excercises.streams;
 
 import com.omju.excercises.reference_operator.Students;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import com.util.UtilStudents;
+import java.util.List;
 
 public class StreamPractice {
     public static void main(String[] args) {
         try {
             Stream<Students> students = UtilStudents.getRandomStudents(52);
-            students.filter(student -> student.getNote() >= 6.0).map(students1 -> students1.getName() + ": Congratulations!!").distinct().forEach(System.out::println);
+            List<String> studentsApproved = students.filter(student -> student.getNote() >= 6.0).map(students1 -> students1.getName() + ": Approved.").distinct().collect(Collectors.toList());
+
+            System.out.println("Students approved: " + studentsApproved.size());
+            studentsApproved.forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
